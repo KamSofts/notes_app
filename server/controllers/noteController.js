d923cc0d67c18c763b77dc3819ca876af093ef3d
KamSofts <kamsofts@gmail.com>
2026-03-20 15:23:11 +0000

    Fetch all user's notes using GET method

--- server/controllers/noteController.js
+++ server/controllers/noteController.js
@@ -17,4 +17,19 @@ const createNote = async (req, res) => {
     }
 };
 
-module.exports = { createNote };
+const readNotes = async (req, res) => {
+    try {
+        const user_id = req.user.id;
+        if (user_id <= 0) {
+            throw new Error("User not found");
+        }
+        const [rs] = await db.query(`SELECT note_id,note
+            ,DATE_FORMAT(date,'%d-%b-%Y') AS date
+             FROM notes WHERE user_id=?;`, [user_id]);
+        res.status(200).json(rs);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+module.exports = { createNote, readNotes };
--- server/routes/noteRoutes.js
+++ server/routes/noteRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const auth = require('../middleware/auth');
-const { createNote } = require('../controllers/noteController');
+const { createNote, readNotes } = require('../controllers/noteController');
 
 const router = express.Router();
 
 router.post("/", auth, createNote);
+router.get("/", auth, readNotes);
 
 module.exports = router;
